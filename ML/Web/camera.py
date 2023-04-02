7d4419bcc71c2c420e82031fd6ad46dad63a0b75
Chamith <chamith.eos@gmail.com>
2023-04-02 21:04:55 +0530

    Commited on 2/4/23

---
+++ ML/Web/camera.py
@@ -0,0 +1,38 @@
+import cv2
+from model import FacialExpressionModel
+import numpy as np
+from datetime import datetime
+
+facec = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+model = FacialExpressionModel("model.json", "model_weights.h5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    # returns camera frames along with bounding boxes and predictions
+    def get_frame(self):
+        _, fr = self.video.read()
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (48, 48))
+            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+            # Write the current time and predicted emotion to the text file
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            with open('emotion_log.txt', 'a') as f:
+                f.write(f'{timestamp},{pred}\n')
+
+            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        _, jpeg = cv2.imencode('.jpg', fr)
+        return jpeg.tobytes()
